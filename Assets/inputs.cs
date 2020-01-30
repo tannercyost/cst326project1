edfdc2e735e0b1395cf27c687450dcd4ebd8270c
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-01-29 20:21:47 -0800

    Updated inputs

---
+++ Assets/inputs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputs : MonoBehaviour
+{
+    public float speed = 10.0f;
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical1") * speed;
+
+        translation *= Time.deltaTime; // put in terms of time instead of frames
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+    }
+}
