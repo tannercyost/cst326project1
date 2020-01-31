bdf6aa6dd22c74f8073e21504c4aedb67047dd6e
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-01-30 19:59:28 -0800

    Finished left and right paddle movement

--- Assets/inputs.cs
+++ Assets/inputs.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class inputs : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 10;
+    public Transform paddle;
+    
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical1") * speed;
+        if (Input.GetAxis("Vertical1") != 0 && paddle.name == "PaddleLeft") // detects WS and triggers left
+        {
+            float translation = Input.GetAxis("Vertical1") * speed;
 
-        translation *= Time.deltaTime; // put in terms of time instead of frames
+            translation *= Time.deltaTime; // put in terms of time instead of frames
 
-        // Move translation along the object's z-axis
-        transform.Translate(0, 0, translation);
+            // Move translation along the object's z-axis
+            transform.Translate(0, 0, translation);
+        }
 
-        // Rotate around our y-axis
+        if (Input.GetAxis("Vertical2") != 0 && paddle.name == "PaddleRight") // detects up/down and triggers right
+        {
+            float translation = Input.GetAxis("Vertical2") * speed;
+
+            translation *= Time.deltaTime; // put in terms of time instead of frames
+
+            // Move translation along the object's z-axis
+            transform.Translate(0, 0, translation);
+        }
     }
 }
