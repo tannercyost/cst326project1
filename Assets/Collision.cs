ffdfa786227461eb2512412456555e1cd8fba913
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-02-02 16:01:21 -0800

    Implemented primitive ball collision with paddle

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class collision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (collision.gameObject.name == "PaddleRight")
+        {
+            Debug.Log("Test paddle right");
+        }
     }
 }
---
+++ Assets/ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    int modifier = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody ball = transform.gameObject.GetComponent<Rigidbody>();
+        ball.AddForce(Vector3.right * modifier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody ball = transform.gameObject.GetComponent<Rigidbody>();
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (collision.gameObject.name == "PaddleRight")
+        {
+            Debug.Log("Right paddle collision");
+            ball.AddForce(Vector3.left * modifier);
+        }
+        if (collision.gameObject.name == "PaddleLeft")
+        {
+            Debug.Log("Left paddle collision");
+            ball.AddForce(Vector3.right * modifier);
+        }
+        modifier += 5;
+    }
+}
